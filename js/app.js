4dec6f6d6f48399d8be7c404c2875a755393b9e5
ricardo <martinez_ric@hotmail.com>
2022-08-08 19:58:03 +0200

    tablet view

--- js/app.js
+++ js/app.js
@@ -5,9 +5,18 @@ function openNav() {
   btn.addEventListener("click", () => {
     nav.classList.add("show-nav");
     nav.classList.remove("hide-nav");
+  });
+}
 
-    console.log(nav);
+function closeNav() {
+  const btn = document.querySelector("#nav-close");
+  const nav = document.querySelector("#main-nav");
+
+  btn.addEventListener("click", () => {
+    nav.classList.add("hide-nav");
+    nav.classList.remove("show-nav");
   });
 }
 
 openNav();
+closeNav();
